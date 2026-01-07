fab535bb4ad53dad19264ac8593129fde3b4c787
Ludwig Lundin Liljeholm <ludlil@kth.se>
2026-01-07 17:47:46 +0100

    update missile sleigh

--- src/fuzzypack/data/weapons/missile_sleigh_onfire.java
+++ src/fuzzypack/data/weapons/missile_sleigh_onfire.java
@@ -9,16 +9,21 @@ import java.util.List;
 public class missile_sleigh_onfire implements OnFireEffectPlugin {
     @Override
     public void onFire(DamagingProjectileAPI projectile, WeaponAPI weapon, CombatEngineAPI engine) {
+        engine.removeEntity(projectile);
         ShipAPI carrier = weapon.getShip().getWing().getSourceShip();
+        if (carrier == null) return;
         List<WeaponAPI> wpnList = carrier.getAllWeapons();
         List<WeaponAPI> foundWpns = new ArrayList<>();
         for (WeaponAPI wpn : wpnList) {
             if (wpn.getType() == WeaponAPI.WeaponType.MISSILE) {
-                foundWpns.add(wpn);
+                if (wpn.getAmmo() > 0) foundWpns.add(wpn);
             }
         }
-        if (foundWpns.isEmpty()) return;
-        String wpnId = foundWpns.get(MathUtils.getRandomNumberInRange(0, foundWpns.size()-1)).getId();
-        engine.spawnProjectile(weapon.getShip(), weapon, wpnId, weapon.getFirePoint(0), weapon.getCurrAngle(), weapon.getShip().getVelocity());
+        if (foundWpns.isEmpty()) {
+            engine.spawnProjectile(weapon.getShip(), weapon, "bomb", weapon.getFirePoint(0), weapon.getCurrAngle(), weapon.getShip().getVelocity());
+        } else {
+            String wpnId = foundWpns.get(MathUtils.getRandomNumberInRange(0, foundWpns.size() - 1)).getId();
+            engine.spawnProjectile(weapon.getShip(), weapon, wpnId, weapon.getFirePoint(0), weapon.getCurrAngle(), weapon.getShip().getVelocity());
+        }
     }
 }
